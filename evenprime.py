89e5592f558fb0a6902a97f1fc1f2e152828696c
Soumya-ui-bytes <soumyarajagopal027@gmail.com>
2025-10-04 10:43:54 +0530

    saved

--- evenprime.py
+++ evenprime.py
@@ -1,6 +1,25 @@
-num  = int(input("Enter a number: "))
+even_count = 0
+odd_count = 0
+prime_count = 0
 
-if num % 2 == 0:
-    print(f"{num} is even")
-else:
-    print(f"{num} is odd")
+start = int(input("Enter the start of the range: "))
+end = int(input("Enter the end of the range: "))
+
+for num in range(start, end + 1):
+    if num % 2 == 0:
+        even_count += 1
+    else:
+        odd_count += 1
+
+    if num > 1:
+        is_prime = True
+        for i in range(2, int(num ** 0.5) + 1):
+            if num % i == 0:
+                is_prime = False
+                break
+        if is_prime:
+            prime_count += 1
+
+print(f"Even numbers: {even_count}")
+print(f"Odd numbers: {odd_count}")
+print(f"Prime numbers: {prime_count}")
